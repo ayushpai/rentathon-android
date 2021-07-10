e80fcd5e94175e0ddf577f16c820879ef00fc0ba
Alex Kranias <alexander.kranias@gmail.com>
2021-07-10 01:54:25 -0400

    new xml file names and stuff

--- app/src/main/java/com/neuralgorithmic/rentathon/MessagingHome.java
+++ app/src/main/java/com/neuralgorithmic/rentathon/MessagingHome.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.neuralgorithmic.rentathon.R;
-
 public class MessagingHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.msg_home);
+        setContentView(R.layout.message_home);
     }
 }
